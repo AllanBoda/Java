f796ae62ecfa564089b064347076bbba963a7549
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2022-11-14 21:42:13 -0300

    Update exercicio.js

--- exercicio.js
+++ exercicio.js
@@ -1,38 +1,19 @@
-console.log('03 - Farenheint ara Celsius');
 
-function farPracel(tempFar){
-    //Recebe por parâmero uma temperatura em graus Far
-    //Retorna Celcius
 
-    let cel = 5*(tempFar-32)/9;
-    
-    return cel;
+function maioEmenor(numeros){
+let maior=-99999, menor=99999;
+
+    for (let numero of numeros){
+    if(numero < menor){
+        menor = numero;
+    }
+    if(numero > maior){
+        maior = numero;
+    }
+}
+return maior-menor;
 }
 
-function celPrafar(tempCel){
-    //Recebe por parâmero uma temperatura em graus Far
-    //Retorna Celcius
+const numeros = [43, 44, 31, 33, 32];
 
-    let far = ((tempCel*9)+160)/5;
-    
-    return far;
-}
-
-function AreaC(areaC){
-    //Recebe por parâmero uma temperatura em graus Far
-    //Retorna Celcius
-
-    let total = 3.14*(areaC*areaC);
-    
-    return total;
-}
-
-let temp = farPracel(100);
-let tempC= celPrafar(100);
-let area = AreaC(100);
-
-console.log('100 graus F são ' + temp + 
-' graus Celsius');
-console.log('100 graus C são ' + tempC + 
-' graus Far');
-console.log('Área do círculo é '+area);
+console.log('A diferença entre os números é: '+maioEmenor(numeros));
